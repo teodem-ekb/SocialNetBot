38ce4e69894f60932ca22f11390aa2bb08097841
Anton Peskov <teodem@weburg.me>
2019-04-17 18:50:49 +0500

    Добавьте файлы проекта.

---
+++ SocialNetBot.Ui/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SocialNetBot.Ui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
